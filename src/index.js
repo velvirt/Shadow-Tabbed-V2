a1d9a9cb08057f93f4f947046dae03892dfb407e
NCCoder3 <136212903+NCCoder3@users.noreply.github.com>
2023-06-13 03:24:21 +0000

    port 80

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ server.on("upgrade", (req, socket, head) => {
 
 let port = parseInt(process.env.PORT || "");
 
-if (isNaN(port)) port = 8080;
+if (isNaN(port)) port = 80;
 
 server.listen(port, () => {
   const address = server.address();
