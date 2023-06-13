9dfa094bb93a4c9e58d904c80638a98606fe102b
NCCoder3 <136212903+NCCoder3@users.noreply.github.com>
2023-06-13 03:23:30 +0000

    Made it x2 Faster

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
 import createBareServer from "@tomphttp/bare-server-node";
 import express from "express";
-import { createServer } from "node:http";
+import { createServer } from "http";
 import { publicPath } from "ultraviolet-static";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
-import { join } from "node:path";
-import { hostname } from "node:os";
+import { join } from "path";
+import { hostname } from "os";
 
 const bare = createBareServer("/bare/");
 const app = express();
 
-// Load our publicPath first and prioritize it over UV.
+// Load the publicPath directory
 app.use(express.static(publicPath));
-// Load vendor files last.
-// The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
-app.use("/uv/", express.static(uvPath));
 
-// Error for everything else
-app.use((req, res) => {
-  res.status(404);
-  res.sendFile(join(publicPath, "404.html"));
-});
-
-const server = createServer();
-
-server.on("request", (req, res) => {
+const server = createServer((req, res) => {
   if (bare.shouldRoute(req)) {
     bare.routeRequest(req, res);
   } else {
-    app(req, res);
+    app(req, res, (err) => {
+      if (err && err.code === "ENOENT") {
+        res.status(404).sendFile(join(publicPath, "404.html"));
+      } else {
+        res.sendStatus(500);
+      }
+    });
   }
 });
 
@@ -43,11 +38,9 @@ let port = parseInt(process.env.PORT || "");
 
 if (isNaN(port)) port = 8080;
 
-server.on("listening", () => {
+server.listen(port, () => {
   const address = server.address();
 
-  // by default we are listening on 0.0.0.0 (every interface)
-  // we just need to list a few
   console.log("Listening on:");
   console.log(`\thttp://localhost:${address.port}`);
   console.log(`\thttp://${hostname()}:${address.port}`);
@@ -58,16 +51,12 @@ server.on("listening", () => {
   );
 });
 
-// https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html
 process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
 
 function shutdown() {
   console.log("SIGTERM signal received: closing HTTP server");
-  server.close();
-  bare.close();
+  server.close(() => {
+    bare.close();
+  });
 }
-
-server.listen({
-  port,
-});
