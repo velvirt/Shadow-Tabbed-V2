9803a1c55ab5b704042e87eb35ffdc4d221fbe58
NCCoder3 <136212903+NCCoder3@users.noreply.github.com>
2023-06-13 00:47:03 +0000

    Optimized for Time

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ server.on("upgrade", (req, socket, head) => {
 
 let port = parseInt(process.env.PORT || "");
 
-if (isNaN(port)) port = 3030;
+if (isNaN(port)) port = 1000;
 
 server.on("listening", () => {
   const address = server.address();
