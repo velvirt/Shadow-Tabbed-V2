14595f2f4fa2f8caa1c2190181e9381dfa9cbe76
NCCoder3 <136212903+NCCoder3@users.noreply.github.com>
2023-06-13 22:25:28 -0500

    Update register-sw.js

--- Ultraviolet-Static/public/register-sw.js
+++ Ultraviolet-Static/public/register-sw.js
@@ -1,32 +1,31 @@
 "use strict";
 
-const stockSW = "/uv/sw.js";
-const isServiceWorkerSupported = 'serviceWorker' in navigator;
+(function () {
+  const stockSW = "/uv/sw.js";
+  const isServiceWorkerSupported = 'serviceWorker' in navigator;
 
-async function registerSW() {
-  if (!isServiceWorkerSupported) {
-    throw new Error("Your browser doesn't support service workers.");
+  function isLocalhost() {
+    return (
+      location.hostname === "localhost" ||
+      location.hostname === "127.0.0.1" ||
+      location.hostname.startsWith("192.168.")
+    );
   }
 
-  if (location.protocol !== "https:" && !isLocalhost()) {
-    throw new Error("Service workers can only be registered under HTTPS protocol.");
+  async function registerSW() {
+    if (!isServiceWorkerSupported) {
+      return Promise.reject(new Error("Your browser doesn't support service workers."));
+    }
+
+    if (location.protocol === "https:" || isLocalhost()) {
+      return navigator.serviceWorker.register(stockSW, {
+        scope: __uv$config.prefix,
+      });
+    } else {
+      return Promise.reject(new Error("Service workers cannot be registered without HTTPS."));
+    }
   }
 
-  await navigator.serviceWorker.register(stockSW, {
-    scope: __uv$config.prefix,
-  });
-}
-
-function isLocalhost() {
-  return (
-    location.hostname === "localhost" ||
-    location.hostname === "127.0.0.1" ||
-    location.hostname.startsWith("192.168.")
-  );
-}
-
-// Call the function to register the service worker
-registerSW()
-  .catch((error) => {
-    console.error(error);
-  });
+  // Call the registerSW function after defining it
+  registerSW();
+})();
