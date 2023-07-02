4a9884fe98eb2aefc1e504a1d402dd454517c243
NottPeak <105991975+NottPeak@users.noreply.github.com>
2023-07-02 15:19:56 -0500

    Changed to astroid bare server for temp nowgg bypass (permission from fatgrizzly & flyawaybear)

--- Ultraviolet-Static/public/uv/uv.config.js
+++ Ultraviolet-Static/public/uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "/bare/",
+  bare: "https://astroid.gg/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
