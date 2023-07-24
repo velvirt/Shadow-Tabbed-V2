e039b944914ca65c21eebd4fcd86ec1541694213
NottPeak <105991975+NottPeak@users.noreply.github.com>
2023-07-24 17:33:38 -0500

    Update uv.config.js

--- Ultraviolet-Static/public/uv/uv.config.js
+++ Ultraviolet-Static/public/uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "https://astroid.gg/bare/",
+  bare: "/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
