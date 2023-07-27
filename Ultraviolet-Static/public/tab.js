db49510d86bab7d397a293e0d1ffd71ae20e482b
NCCoder2 <133831293+NCCoder2@users.noreply.github.com>
2023-07-27 05:28:24 +0000

    ds

--- Ultraviolet-Static/public/tab.js
+++ Ultraviolet-Static/public/tab.js
@@ -1,159 +1,179 @@
-const tabsContainer = document.querySelector('.tab-control');
-const iframeContainer = document.getElementById('iframe-container');
-const closeBtn = document.getElementById('closebtn');
-const addBtn = document.querySelector('.open-btn');
+document.addEventListener('DOMContentLoaded', function () {
+  let tabCount = 1;
+  const tabsContainer = document.querySelector('.tabs');
+  const tabPanelsContainer = document.querySelector('.tab-panels');
+  const addTabBtn = document.getElementById('add-tab-btn');
+  const closeTabBtn = document.getElementById('closetabbtn');
+  const iframeControl = document.querySelector('.iframe-control');
+  const inputField = document.querySelector('input');
+  const backBtn = document.querySelector('.back-btn');
+  const refreshBtn = document.querySelector('.refresh-btn');
+  const forwardBtn = document.querySelector('.forward-btn');
+  let currentTabPanelId = null;
 
-let isCloseButtonDisabled = false;
-let tabIframeMap = new Map();
-let tabCounter = 1;
-
-tabsContainer.addEventListener('click', function(event) {
-  const clickedElement = event.target;
-
-  if (clickedElement.classList.contains('tab')) {
-    const isActiveTab = clickedElement.classList.contains('active');
-    if (!isActiveTab) {
-      changeTab(clickedElement);
-    }
-  } else if (clickedElement.classList.contains('close-btn') && !isCloseButtonDisabled) {
-    const tab = clickedElement.parentNode;
-    removeTab(tab);
-  }
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  const homeTab = document.querySelector('.tab');
-  changeTab(homeTab);
-});
-
-function changeTab(selectedTab, src) {
-  const tabs = tabsContainer.querySelectorAll('.tab');
-
-  tabs.forEach(tab => tab.classList.remove('active'));
-  selectedTab.classList.add('active');
-
-  const tabId = selectedTab.dataset.tabId;
-
-  if (tabIframeMap.has(tabId)) {
-    const iframe = tabIframeMap.get(tabId);
-    showIframe(iframe);
-  } else {
-    let iframeSrc;
-    if (src) {
-      iframeSrc = src;
-    } else if (tabId === 'home') {
-      iframeSrc = 'home.html';
-    } else {
-      iframeSrc = selectedTab.dataset.src || 'main.html';
-    }
-
-    const iframe = createIframe(iframeSrc);
-    tabIframeMap.set(tabId, iframe);
-    showIframe(iframe);
-    iframe.addEventListener('load', function () {
-      const title = iframe.contentDocument.title;
-      if (iframe.getAttribute('src') !== 'home.html' && iframe.getAttribute('src') !== 'main.html' && iframe.getAttribute('src') !== '/settings/') {
-        selectedTab.innerHTML = '<i class="fa-solid fa-table-columns fa-lg"></i>' + title;
-        let closeBtn = selectedTab.querySelector('.close-btn-tab');
-        if (!closeBtn) {
-          closeBtn = document.createElement('div');
-          closeBtn.classList.add('close-btn-tab');
-          closeBtn.innerHTML = '<div onclick="closebutton(event)"><i class="fas fa-times"></i></div>';
-          selectedTab.appendChild(closeBtn);
-        }
+  function switchTab(event) {
+      const target = event.target;
+      if (target === addTabBtn) {
+          addTab();
+      } else if (target.classList.contains('close-tab-btn')) {
+          closeTab(event);
+      } else {
+          const tabPanel = target.closest('li').getAttribute('data-panel');
+          showTab(tabPanel);
       }
-    });
   }
-}
 
+ function addTab(title, src) {
+  const newTabId = `tab${tabCount}`;
+  const newTabPanelId = `panel${tabCount}`;
 
-
-function createIframe(src) {
-  const iframe = document.createElement('iframe');
-  iframe.setAttribute('src', src);
-  iframe.style.width = '100%';
-  iframe.setAttribute('id', 'tab-iframe'); 
-  iframe.style.height = '100%';
-  iframe.style.display = 'none';
-  iframeContainer.appendChild(iframe);
-  return iframe;
-}
-
-function showIframe(iframe) {
-  iframeContainer.querySelectorAll('iframe').forEach(iframe => {
-    iframe.style.display = 'none';
+  document.querySelectorAll('.tabs li').forEach(tab => {
+      tab.classList.remove('active');
   });
-  iframe.style.display = 'block';
+
+  document.querySelectorAll('.tab-panel').forEach(panel => {
+      panel.classList.remove('active');
+  });
+
+  const newTab = document.createElement('li');
+  newTab.setAttribute('data-panel', newTabPanelId);
+  newTab.classList.add('active');
+  newTab.innerHTML = `${title} <span class="close-tab-btn"></span>`;
+  tabsContainer.appendChild(newTab);
+
+  const newTabPanel = document.createElement('div');
+  newTabPanel.setAttribute('data-src', src);
+  newTabPanel.classList.add('tab-panel', 'active');
+  newTabPanel.setAttribute('id', newTabPanelId);
+  newTabPanel.innerHTML = `<iframe src="${src}" frameborder="0"></iframe>`;
+  tabPanelsContainer.appendChild(newTabPanel);
+
+  tabCount++;
+  resizeTabs();
 }
 
-function removeTab(tab) {
-  const isCurrentTab = tab.classList.contains('active');
-  const tabId = tab.dataset.tabId;
-  const tabs = tabsContainer.querySelectorAll('.tab');
-  const tabCount = tabs.length;
 
-  if (isCurrentTab) {
-    const previousTab = tab.previousElementSibling;
-    const nextTab = tab.nextElementSibling;
-    const newActiveTab = previousTab || nextTab;
+  function showTab(panelId) {
+      document.querySelectorAll('.tabs li').forEach(tab => {
+          tab.classList.remove('active');
+      });
 
-    if (newActiveTab) {
-      changeTab(newActiveTab);
-    }
+      document.querySelectorAll('.tab-panel').forEach(panel => {
+          panel.classList.remove('active');
+      });
+
+      const tabToActivate = document.querySelector(`.tabs li[data-panel="${panelId}"]`);
+      const tabPanelToActivate = document.querySelector(`#${panelId}`);
+      tabToActivate.classList.add('active');
+      tabPanelToActivate.classList.add('active');
+      currentTabPanelId = panelId;
   }
 
-  iframeContainer.removeChild(tabIframeMap.get(tabId));
-  tabIframeMap.delete(tabId);
-  tab.remove();
+   function closeTab(event) {
+  const activeTabPanel = document.querySelector('.tab-panel.active');
+  if (activeTabPanel) {
+      const tabPanelId = activeTabPanel.getAttribute('id');
+      const tabToRemove = document.querySelector(`[data-panel="${tabPanelId}"]`);
+      const tabPanelToRemove = document.getElementById(tabPanelId);
 
-  if (tabCount === 1) {
-    closeBtn.classList.add('disabled');
-    isCloseButtonDisabled = true;
+      localStorage.removeItem(`${tabPanelId}-input`); 
+
+      tabPanelToRemove.remove(); 
+      tabToRemove.remove();
+
+      const newActiveTabPanel = document.querySelector('.tab-panel.active');
+      if (newActiveTabPanel) {
+          showTab(newActiveTabPanel.getAttribute('id'));
+      } else {
+          const lastTabPanel = document.querySelector('.tab-panel:last-child');
+          if (lastTabPanel) {
+              showTab(lastTabPanel.getAttribute('id'));
+          } else {
+              // No tabs left, show a message to open a new tab
+              const newTabMessage = document.createElement('div');
+              newTabMessage.classList.add('new-tab-message');
+              newTabMessage.textContent = 'Open a new tab to continue';
+              tabPanelsContainer.appendChild(newTabMessage);
+          }
+      }
+
+      resizeTabs();
+  }}
+
+
+
+  function saveInputs(panelId) {
+      const iframe = document.querySelector(`#${panelId} iframe`);
+      const inputs = iframe.contentDocument.querySelectorAll('input, textarea');
+
+      inputs.forEach(input => {
+          const savedValue = localStorage.getItem(`${panelId}-${input.name}`);
+          input.value = savedValue || '';
+
+          input.addEventListener('input', function () {
+              localStorage.setItem(`${panelId}-${input.name}`, input.value);
+          });
+      });
   }
 
-  setTimeout(() => {
-    closeBtn.classList.remove('disabled');
-    isCloseButtonDisabled = false;
-  }, 500);
-}
+  function initTabs() {
+      tabsContainer.addEventListener('click', switchTab);
+      addTabBtn.addEventListener('click', function() {
+        addTab('<i class="fas fa-globe"></i> New Tab', "https://ludicrous.icu/"); 
+      });
 
-closeBtn.addEventListener('click', function() {
-  const activeTab = tabsContainer.querySelector('.tab.active');
-  const tabs = tabsContainer.querySelectorAll('.tab');
-  if (tabs.length === 1) {
-    return;
-  }   
+      inputField.addEventListener('keydown', function (event) {
+          if (event.key === 'Enter') {
+              const iframe = document.querySelector(`#${currentTabPanelId} iframe`);
+              const url = inputField.value.trim();
+              iframe.src = url;
+          }
+      });
 
-  if (activeTab) {
-    removeTab(activeTab);
+      const closeBtn = document.getElementById('closetabbtn');
+      closeBtn.addEventListener('click', closeTab);
+
+      backBtn.addEventListener('click', function () {
+          const iframe = document.querySelector(`#${currentTabPanelId} iframe`);
+          iframe.contentWindow.history.back();
+      });
+
+      refreshBtn.addEventListener('click', function () {
+          const iframe = document.querySelector(`#${currentTabPanelId} iframe`);
+          iframe.contentWindow.location.reload();
+      });
+
+      forwardBtn.addEventListener('click', function () {
+          const iframe = document.querySelector(`#${currentTabPanelId} iframe`);
+          iframe.contentWindow.history.forward();
+      });
+
+      
+
+      showTab(`panel1`);
+      saveInputs(`panel1`);
   }
+
+  function resizeTabs() {
+      const tabWidth = 100; // Set the width of each tab (change as needed)
+      const totalTabs = tabsContainer.childElementCount;
+      const availableWidth = tabsContainer.offsetWidth;
+      const maxVisibleTabs = Math.floor(availableWidth / tabWidth);
+      const visibleTabs = Math.min(maxVisibleTabs, totalTabs);
+
+      const newTabWidth = `${100 / visibleTabs}%`;
+      tabsContainer.querySelectorAll('.tabs li').forEach(tab => {
+          tab.style.width = newTabWidth;
+      });
+  }
+
+  // Handle window resize to adjust the tabs
+  window.addEventListener('resize', function () {
+      resizeTabs();
+  });
+  window.onload = function() {
+    addTab('<i class="fa-solid fa-house"></i> Home', "home.html"); 
+    document.getElementById('search').value = 'shadow://home'
+  };
+  initTabs();
 });
-
-function addTab(title) {
-  const tabManager = document.querySelector('.tab-manager');
-  const tabId = tabCounter++;
-
-  const newTab = document.createElement('div');
-  newTab.classList.add('tab');
-  newTab.dataset.tabId = tabId;
-  newTab.innerHTML = title || '<i class="fa-solid fa-table-columns fa-lg"></i> New Tab';
-  tabManager.insertAdjacentElement('beforebegin', newTab);
-
-  const iframe = createIframe('');
-  iframe.dataset.tabId = tabId;
-  tabIframeMap.set(tabId, iframe);
-}
-
-
-  function closebutton(){
-      const activeTab = tabsContainer.querySelector('.tab.active');
-      const tabs = tabsContainer.querySelectorAll('.tab');
-    
-      if (tabs.length === 1) {
-        return;
-      }
-    
-      if (activeTab) {
-        removeTab(activeTab);
-      }
-  }
